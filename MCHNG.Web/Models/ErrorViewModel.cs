14938a59588f9f2e7517c43f6e398e3423bb705d
ALEJANDRA NAVARRO LAZCANO <alejandra.navarrola@my.uvm.edu.mx>
2019-10-16 17:09:20 -0500

    Creación de .Web

---
+++ MCHNG.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MCHNG.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
